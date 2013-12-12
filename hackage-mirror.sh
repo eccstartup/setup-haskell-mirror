1da3290f3746d4dd332d4de5d7863e5d7cc9e096
snowXman <zhiwudazhanjiangshi@gmail.com>
2013-12-12 13:23:48 +0800

    Update hackage-mirror.sh
    modified from https://gist.github.com/eccstartup/7889417

--- hackage-mirror.sh
+++ hackage-mirror.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /home/luyi/mirror/hackage
+cd /home/eccstartup/mirror/hackage
 echo "Cleaning up..."
 # rm 00-index.tar.gz
 mkdir -p package
 echo "Downloading index..."
-wget -c http://hdiff.luite.com/packages/archive/00-index.tar.gz -O index.tar.gz
+wget -c http://hackage.haskell.org/packages/archive/00-index.tar.gz -O index.tar.gz
 for splitpk in `tar ztf index.tar.gz | cut -d/ -f 1,2 2>/dev/null`; do
 	pk=`echo $splitpk | sed 's|/|-|'`
 	name=$pk.tar.gz
